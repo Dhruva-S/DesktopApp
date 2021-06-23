85045f5f726b799b6cad6fc442a03da8c7255cee
Dhruva-S <dhruvasree296@gmail.com>
2021-06-24 00:32:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,109 @@
+const express=require("express");//require express module
+const path=require('path'); 
+const { ServerResponse } = require("http");
+const https=require("https");
+const app=express();//initializes a new express app
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); 
+
+
+//Listening on the default port for a hosting server and on port 3000 for the localhost 
+app.listen(process.env.PORT||3000,function(req,res){
+    console.log("server has started om port 3000");
+});
+
+
+app.use(express.static(__dirname + '/public'));
+var wd = require("word-definition");
+// app.get("/",function(req,res){
+
+//     res.sendFile(__dirname+"/index.html");
+// });
+
+app.get("/",function(req,res){
+    // const query=req.body.cityName;
+    const query="Mangalore";
+   const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&Appid=709116cfdf42b242a173d864e3de0d7f&units=metric";
+   https.get(url,function(response){
+   console.log(response.statusCode);
+   
+   response.on("data",function(data){
+       const weatherData=JSON.parse(data);
+       const temp=weatherData.main.temp;
+       const weatherDescription=weatherData.weather[0].description;
+       const icon=weatherData.weather[0].icon;
+       
+       const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+       console.log(weatherDescription);
+       
+    //    res.write("<p>The weather is : "+ weatherDescription+" </p>");
+    //    res.write("<h1> The temperature is:"+temp+" degrees celsius</h1>");
+    //    res.write("<img src="+imageURL+">");
+    //    res.send();
+
+       res.render('landing', { 
+        temp: temp,
+        weatherDescription: weatherDescription,
+        imageURL :imageURL
+        });
+   });
+   
+   });
+   });
+// app.get("/",function(req,res){
+//     // res.sendFile(__dirname+"/html/myeditor.html");
+//     res.sendFile(path.join(__dirname,'','index.html'));
+// });
+
+app.get("/html/myeditor.html",function(req,res){
+    // res.sendFile(__dirname+"/html/myeditor.html");
+    res.sendFile(path.join(__dirname,'html','myeditor.html'));
+});
+
+
+app.get("/ToDoList/index.html",function(req,res){
+    // res.sendFile(__dirname+"/html/myeditor.html");
+    res.sendFile(path.join(__dirname,'ToDoList','index.html'));
+});
+
+
+app.get("/html/news.html",function(req,res){
+    // res.sendFile(__dirname+"/html/myeditor.html");
+    res.sendFile(path.join(__dirname,'html','news.html'));
+});
+app.get("/images/list.jpg",function(req,res){
+    // res.sendFile(__dirname+"/html/myeditor.html");
+    res.sendFile(path.join(__dirname,'images','list.jpg'));
+});
+
+app.get("/images/news.jpg",function(req,res){
+    // res.sendFile(__dirname+"/html/myeditor.html");
+    res.sendFile(path.join(__dirname,'images','news.jpg'));
+});
+
+app.get("/images/EasyText.png",function(req,res){
+    // res.sendFile(__dirname+"/html/myeditor.html");
+    res.sendFile(path.join(__dirname,'images','EasyText.png'));
+});
+
+app.post("/",function(req,res){
+    wd.getDef(req.body.word, "en", null, function(word) {
+         
+        //  console.log(typeof(ans));
+        console.log(word.definition);
+    //     res.write("<p>The definition is "+ ans+" </p>");
+    //    res.send();
+    var name=word.word;
+    var category=word.category;
+    var definition=word.definition;
+   
+            res.render('searchResult', { 
+                name: name,
+                category: category,
+                definition :definition
+                });
+    });
+   });
+   
---
+++ public/javascript/news.js
@@ -0,0 +1,52 @@
+function getdata(topic){
+    document.getElementById("topicDisplay").innerHTML=localStorage.getItem('userTopic');
+    console.log(topic);
+    var url = 'https://newsapi.org/v2/everything?' +
+             'q='+topic+'&' +
+             'from=2021-06-22&' +
+             'sortBy=popularity&' +
+             'apiKey=0917198423514d8c82a41272ff93c662';
+   
+    // var url="https://newsapi.org/v2/top-headlines?country=us&apiKey=0917198423514d8c82a41272ff93c662";      //Top Headlines from india
+   var req = new Request(url);
+   
+   fetch(req)
+       .then(function(response) {
+            // console.log(response.json());
+           return response.json();
+       })
+       .then(data=> {
+           console.log(data);  
+           const html=data.articles.map(article=>{
+           return  `
+           <div class="news" >
+           <a href="${article.url}"<a>
+           <p><img src="${article.urlToImage}" width="200px" height="100px" alt="Article image" /></p>
+           <h3>Author:</h3> ${article.author}
+           <h3>Content:</h3>${article.content}`
+           +"</div>"
+           })
+           console.log(html);
+           document.getElementById("text").innerHTML=html;
+       });  
+       }
+   
+       window.onload = function() {
+        if(localStorage.getItem("userTopic") === null){
+            localStorage.setItem('userTopic',"Headlines");
+        };
+        
+        document.getElementById("userTopic").value=localStorage.getItem('userTopic');
+        var Topic=document.getElementById("userTopic").value;
+       getdata(Topic);
+      };
+   function callGetdata(){
+       alert("i'm in!");
+       var Topic=document.getElementById("userTopic").value;
+       console.log("here:"+Topic);
+       alert("Topic: "+Topic);
+       localStorage.setItem('userTopic', document.getElementById('userTopic').value);
+       document.getElementById("userTopic").value=localStorage.getItem('userTopic');
+       getdata(Topic);
+   }
+
